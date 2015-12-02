a690a07d17f8c6e6fb74246d4365205f80926cbc
Simon Berggren <Simon.berggren581@gmail.com>
2015-12-02 14:25:39 +0100

    Initial commit

---
+++ Assignment3/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Assignment3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StorageProgressBar = new System.Windows.Forms.ProgressBar();
+            this.StorageLabel = new System.Windows.Forms.Label();
+            this.FoodFactoryBLabel = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FactoryBStatusLabel = new System.Windows.Forms.Label();
+            this.FactoryBProducerLabel = new System.Windows.Forms.Label();
+            this.FactoryBStartWorkingButton = new System.Windows.Forms.Button();
+            this.FactoryBStopWorkingButton = new System.Windows.Forms.Button();
+            this.FactoryAStopWorkingButton = new System.Windows.Forms.Button();
+            this.FactoryAStartWorkingButton = new System.Windows.Forms.Button();
+            this.FactoryAProducerLabel = new System.Windows.Forms.Label();
+            this.FactoryAStatusLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.StartDeliverButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StorageProgressBar
+            // 
+            this.StorageProgressBar.Location = new System.Drawing.Point(27, 51);
+            this.StorageProgressBar.Name = "StorageProgressBar";
+            this.StorageProgressBar.Size = new System.Drawing.Size(193, 53);
+            this.StorageProgressBar.TabIndex = 0;
+            // 
+            // StorageLabel
+            // 
+            this.StorageLabel.AutoSize = true;
+            this.StorageLabel.Location = new System.Drawing.Point(24, 24);
+            this.StorageLabel.Name = "StorageLabel";
+            this.StorageLabel.Size = new System.Drawing.Size(44, 13);
+            this.StorageLabel.TabIndex = 1;
+            this.StorageLabel.Text = "Storage";
+            // 
+            // FoodFactoryBLabel
+            // 
+            this.FoodFactoryBLabel.AutoSize = true;
+            this.FoodFactoryBLabel.Location = new System.Drawing.Point(360, 24);
+            this.FoodFactoryBLabel.Name = "FoodFactoryBLabel";
+            this.FoodFactoryBLabel.Size = new System.Drawing.Size(79, 13);
+            this.FoodFactoryBLabel.TabIndex = 2;
+            this.FoodFactoryBLabel.Text = "Food Factory B";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(360, 51);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(40, 13);
+            this.Label1.TabIndex = 3;
+            this.Label1.Text = "Status:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Producer:";
+            // 
+            // FactoryBStatusLabel
+            // 
+            this.FactoryBStatusLabel.AutoSize = true;
+            this.FactoryBStatusLabel.Location = new System.Drawing.Point(419, 51);
+            this.FactoryBStatusLabel.Name = "FactoryBStatusLabel";
+            this.FactoryBStatusLabel.Size = new System.Drawing.Size(35, 13);
+            this.FactoryBStatusLabel.TabIndex = 5;
+            this.FactoryBStatusLabel.Text = "NULL";
+            // 
+            // FactoryBProducerLabel
+            // 
+            this.FactoryBProducerLabel.AutoSize = true;
+            this.FactoryBProducerLabel.Location = new System.Drawing.Point(419, 64);
+            this.FactoryBProducerLabel.Name = "FactoryBProducerLabel";
+            this.FactoryBProducerLabel.Size = new System.Drawing.Size(35, 13);
+            this.FactoryBProducerLabel.TabIndex = 6;
+            this.FactoryBProducerLabel.Text = "NULL";
+            // 
+            // FactoryBStartWorkingButton
+            // 
+            this.FactoryBStartWorkingButton.Location = new System.Drawing.Point(363, 119);
+            this.FactoryBStartWorkingButton.Name = "FactoryBStartWorkingButton";
+            this.FactoryBStartWorkingButton.Size = new System.Drawing.Size(105, 23);
+            this.FactoryBStartWorkingButton.TabIndex = 7;
+            this.FactoryBStartWorkingButton.Text = "Start Working";
+            this.FactoryBStartWorkingButton.UseVisualStyleBackColor = true;
+            // 
+            // FactoryBStopWorkingButton
+            // 
+            this.FactoryBStopWorkingButton.Location = new System.Drawing.Point(490, 119);
+            this.FactoryBStopWorkingButton.Name = "FactoryBStopWorkingButton";
+            this.FactoryBStopWorkingButton.Size = new System.Drawing.Size(75, 23);
+            this.FactoryBStopWorkingButton.TabIndex = 8;
+            this.FactoryBStopWorkingButton.Text = "Stop";
+            this.FactoryBStopWorkingButton.UseVisualStyleBackColor = true;
+            // 
+            // FactoryAStopWorkingButton
+            // 
+            this.FactoryAStopWorkingButton.Location = new System.Drawing.Point(490, 267);
+            this.FactoryAStopWorkingButton.Name = "FactoryAStopWorkingButton";
+            this.FactoryAStopWorkingButton.Size = new System.Drawing.Size(75, 23);
+            this.FactoryAStopWorkingButton.TabIndex = 15;
+            this.FactoryAStopWorkingButton.Text = "Stop";
+            this.FactoryAStopWorkingButton.UseVisualStyleBackColor = true;
+            // 
+            // FactoryAStartWorkingButton
+            // 
+            this.FactoryAStartWorkingButton.Location = new System.Drawing.Point(363, 267);
+            this.FactoryAStartWorkingButton.Name = "FactoryAStartWorkingButton";
+            this.FactoryAStartWorkingButton.Size = new System.Drawing.Size(105, 23);
+            this.FactoryAStartWorkingButton.TabIndex = 14;
+            this.FactoryAStartWorkingButton.Text = "Start Working";
+            this.FactoryAStartWorkingButton.UseVisualStyleBackColor = true;
+            // 
+            // FactoryAProducerLabel
+            // 
+            this.FactoryAProducerLabel.AutoSize = true;
+            this.FactoryAProducerLabel.Location = new System.Drawing.Point(419, 221);
+            this.FactoryAProducerLabel.Name = "FactoryAProducerLabel";
+            this.FactoryAProducerLabel.Size = new System.Drawing.Size(35, 13);
+            this.FactoryAProducerLabel.TabIndex = 13;
+            this.FactoryAProducerLabel.Text = "NULL";
+            // 
+            // FactoryAStatusLabel
+            // 
+            this.FactoryAStatusLabel.AutoSize = true;
+            this.FactoryAStatusLabel.Location = new System.Drawing.Point(419, 208);
+            this.FactoryAStatusLabel.Name = "FactoryAStatusLabel";
+            this.FactoryAStatusLabel.Size = new System.Drawing.Size(35, 13);
+            this.FactoryAStatusLabel.TabIndex = 12;
+            this.FactoryAStatusLabel.Text = "NULL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(360, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Producer:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(360, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Status:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(360, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Food Factory A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Delivery to ICA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Truck Cargo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 189);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Truck limits:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(24, 222);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Weight: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(24, 235);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(45, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Volume:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(24, 248);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Items:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(64, 272);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(104, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "New Truck: Loading";
+            // 
+            // StartDeliverButton
+            // 
+            this.StartDeliverButton.Location = new System.Drawing.Point(12, 319);
+            this.StartDeliverButton.Name = "StartDeliverButton";
+            this.StartDeliverButton.Size = new System.Drawing.Size(75, 23);
+            this.StartDeliverButton.TabIndex = 23;
+            this.StartDeliverButton.Text = "Start Deliver";
+            this.StartDeliverButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 354);
+            this.Controls.Add(this.StartDeliverButton);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FactoryAStopWorkingButton);
+            this.Controls.Add(this.FactoryAStartWorkingButton);
+            this.Controls.Add(this.FactoryAProducerLabel);
+            this.Controls.Add(this.FactoryAStatusLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.FactoryBStopWorkingButton);
+            this.Controls.Add(this.FactoryBStartWorkingButton);
+            this.Controls.Add(this.FactoryBProducerLabel);
+            this.Controls.Add(this.FactoryBStatusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.FoodFactoryBLabel);
+            this.Controls.Add(this.StorageLabel);
+            this.Controls.Add(this.StorageProgressBar);
+            this.Name = "MainForm";
+            this.Text = "ICA Delivery System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar StorageProgressBar;
+        private System.Windows.Forms.Label StorageLabel;
+        private System.Windows.Forms.Label FoodFactoryBLabel;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label FactoryBStatusLabel;
+        private System.Windows.Forms.Label FactoryBProducerLabel;
+        private System.Windows.Forms.Button FactoryBStartWorkingButton;
+        private System.Windows.Forms.Button FactoryBStopWorkingButton;
+        private System.Windows.Forms.Button FactoryAStopWorkingButton;
+        private System.Windows.Forms.Button FactoryAStartWorkingButton;
+        private System.Windows.Forms.Label FactoryAProducerLabel;
+        private System.Windows.Forms.Label FactoryAStatusLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button StartDeliverButton;
+    }
+}
+
